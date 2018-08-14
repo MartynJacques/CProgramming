b314b7c033fa2ce9289646df10522f621be9dc6c
Martyn Jacques <martyn.jacques@arm.com>
2018-08-14 11:31:11 +0100

    done ex3-02

--- Chapter3/ex3-02/switch.c
+++ Chapter3/ex3-02/switch.c
@@ -1,12 +1,70 @@
-/* Write a function espace(s, t) that converts characters like newline and tab
- * into visible escape sequences like \n and \t as it copies the string t to s.
- * Use a switch. Write a function for the other direction as well, converting
- * escape sequences into the real characters.
- */
-
 #include <stdio.h>
 
-int main() {
+#define MAX_LEN 50
 
+void escape(char s[], char t[]) {
+    int i = 0;
+    int j = 0;
+    while (s[i] != '\0') {
+        switch(s[i]) {
+            case('\n'):
+                t[j] = '\\';
+                t[j+1] = 'n';
+                j += 2;
+                break;
+            case('\t'):
+                t[j] = '\\';
+                t[j+1] = 't';
+                j += 2;
+                break;
+            default:
+                t[j] = s[i];
+                j++;
+                break;
+        }
+        i++;
+    }
+}
+
+void capture(char s[], char t[]) {
+    int i = 0;
+    int j = 0;
+    while (s[i] != '\0') {
+        switch(s[i]) {
+            case('\\'):
+                switch(s[i+1]) {
+                    case('n'):
+                        t[j] = '\n';
+                        i += 2;
+                        j++;
+                        break;
+                    default:
+                        t[j] = s[i];
+                        i++;
+                        j++;
+                        break;
+                }
+                break;
+            default:
+                t[j] = s[i];
+                j++;
+                i++;
+                break;
+        }
+    }
+    t[j] = '\0';
+}
+
+int main() {
+    char input[] = "This is \t a \n test";
+    printf("%s\n", input);
+    char output[MAX_LEN];
+
+    escape(input, output);
+    printf("%s\n", output);
+
+    char final[MAX_LEN];
+    capture(output, final);
+    printf("%s\n", final);
 }
 
