0ed92ef953b959cdbc86e2a8ef6c35ec1f9285b0
Martyn Jacques <martyn.jacques@arm.com>
2018-08-16 15:26:33 +0100

    added files some not done

---
+++ Chapter3/ex3-03/expand.c
@@ -0,0 +1,18 @@
+// TODO
+#include <stdio.h>
+
+#define MAX 50
+
+void expand(char s[], char t[]) {
+
+}
+
+int main() {
+    char input[] = "Expand a-f that";
+    char output[MAX];
+
+    printf("%s\n", input);
+    expand(input, output);
+    printf("%s", output);
+}
+
